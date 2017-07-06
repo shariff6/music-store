d58c0a06c9f47acfd9dcd1670126cde0783daa24
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-06 10:31:59 +0300

    tests if the clear function empties out the array

--- spec/music_store_spec.rb
+++ spec/music_store_spec.rb
@@ -6,19 +6,19 @@ describe("CD") do
     CD.clear
   end
 
-  describe("name") do
+  describe("#name") do
     it "returns the name of the CD" do
       test_cd = CD.new("garrix", nil, 2000)
       expect(test_cd.name).to(eq("garrix"))
     end
   end
-  describe("image") do
+  describe("#image") do
     it "returns the image of the CD" do
       test_cd = CD.new("garrix", nil, 2000)
       expect(test_cd.image).to(eq(nil))
     end
   end
-  describe("year") do
+  describe("#year") do
     it "returns the year of the CD" do
       test_cd = CD.new("garrix", nil, 2000)
       expect(test_cd.year).to(eq(2000))
@@ -34,6 +34,13 @@ describe("CD") do
       test_cd = CD.new("garrix", nil, 2000)
       test_cd.save
       expect(CD.all).to(eq([test_cd]))
+    end
+  end
+  describe(".clear") do
+    it "clears the saved CDs" do
+      CD.new("garrix", nil, 2000).save
+      CD.clear
+      expect(CD.all).to(eq([]))
     end
   end
 end
