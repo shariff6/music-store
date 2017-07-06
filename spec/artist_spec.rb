14a47cd275c76b23a83fabfe243c3874b039a70b
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-06 12:35:24 +0300

    this commit will add a test for adding an image to an artist

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -7,25 +7,31 @@ describe(Artist) do
   end
   describe('#name') do
     it('returns the name of the artist') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       expect(test_artist.name).to(eq('james'))
+    end
+  end
+  describe('#image') do
+    it('returns the image of the artist') do
+      test_artist = Artist.new('james', nil)
+      expect(test_artist.image).to(eq(nil))
     end
   end
   describe('#id') do
     it('returns the id of the artist') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       expect(test_artist.id).to(eq(1))
     end
   end
   describe('#cds') do
     it('checks if empty array of cds is initially returned') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       expect(test_artist.cds).to(eq([]))
     end
   end
   describe('#save') do
     it('adds an artist to the array of saved artists') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       test_artist.save
       expect(Artist.all).to(eq([test_artist]))
     end
@@ -37,23 +43,23 @@ describe(Artist) do
   end
   describe('.clear') do
     it('empties out all of the saved artists') do
-      Artist.new('james').save
+      Artist.new('james', nil).save
       Artist.clear
       expect(Artist.all).to(eq([]))
     end
   end
   describe('.find') do
     it('returns an artist by their id number') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       test_artist.save
-      test_artist2 = Artist.new('james')
+      test_artist2 = Artist.new('james', nil)
       test_artist2.save
       expect(Artist.find(test_artist.id)).to(eq(test_artist))
     end
   end
   describe('#add_cd') do
     it('adds a new cd to an artist') do
-      test_artist = Artist.new('james')
+      test_artist = Artist.new('james', nil)
       test_cd = CD.new('garrix', nil, 2000)
       test_artist.add_cd(test_cd)
       expect(test_artist.cds).to(eq([test_cd]))
