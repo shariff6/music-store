61736f22a07b70970e5816fb75e072ad4adfd0ed
samwelkinuthia <samwuelkinuthia@gmail.com>
2017-07-06 10:51:24 +0300

    added methods to save, find and clear

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  @@artists = []
+  attr_reader :name, :id, :cds
+  def initialize(name)
+    @name = name
+    @id = @@artists.length.+(1)
+    @cds = []
+  end
+  define_singleton_method(:all) do
+    @@artists
+  end
+  define_method(:save) do
+    @@artists.push(self)
+  end
+  define_singleton_method(:clear) do
+    @@artists = []
+  end
+  define_method(:add_cd) do |cd|
+    @cds.push(cd)
+  end
+  define_singleton_method(:find) do |id|
+    found_artist = nil
+    @@artists.each do |artist|
+      found_artist = artist if artist.id.eql?(id)
+    end
+    found_artist
+  end
+end
